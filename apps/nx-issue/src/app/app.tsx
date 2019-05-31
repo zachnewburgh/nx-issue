409d52fef78328bd3343e6bd544b6c0f19937a30
Zach Newburgh <zach.newburgh@blackrock.com>
2019-05-31 09:47:07 -0400

    feat: use Idea component in App

--- apps/nx-issue/src/app/app.tsx
+++ apps/nx-issue/src/app/app.tsx
@@ -1,9 +1,15 @@
+import { Idea } from '@nx-issue/idea';
 import { ReactUi } from '@nx-issue/react-ui';
 import React from 'react';
 import './app.scss';
 
 export const App = () => {
-  return <ReactUi />;
+  return (
+    <>
+      <ReactUi />
+      <Idea />
+    </>
+  );
 };
 
 export default App;

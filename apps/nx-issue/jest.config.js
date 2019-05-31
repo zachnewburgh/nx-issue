f7e51ad85bb0adfd4d42eede093543503daad4b5
Zach Newburgh <zach.newburgh@blackrock.com>
2019-05-31 09:32:12 -0400

    initial commit

---
+++ apps/nx-issue/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'nx-issue',
+  preset: '../../jest.config.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  coverageDirectory: '../../coverage/apps/nx-issue'
+};

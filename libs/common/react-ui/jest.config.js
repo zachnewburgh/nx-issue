3b793c8e230e7f7fba8557feabe69f20a9745b75
Zach Newburgh <zach.newburgh@blackrock.com>
2019-05-31 09:42:38 -0400

    refactor: move libs/react-ui to libs/common/react-ui

--- libs/common/react-ui/jest.config.js
+++ libs/common/react-ui/jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   name: 'react-ui',
-  preset: '../../jest.config.js',
+  preset: '../../../jest.config.js',
   transform: {
     '^.+\\.[tj]sx?$': 'ts-jest'
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../coverage/libs/react-ui'
+  coverageDirectory: '../../../coverage/libs/common/react-ui'
 };

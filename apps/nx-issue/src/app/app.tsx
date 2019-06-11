368ae7958b2eb9a0184433f6175fc6c6f43044a3
Zach Newburgh <zach.newburgh@blackrock.com>
2019-06-11 12:04:34 -0400

    feat(*): upgrade nx to 8.1.0

--- apps/nx-issue/src/app/app.tsx
+++ apps/nx-issue/src/app/app.tsx
@@ -3,13 +3,15 @@ import { RedocStandalone } from 'redoc';
 import './app.scss';
 
 export const App = () => {
-return (<RedocStandalone
-  specUrl="http://rebilly.github.io/RebillyAPI/openapi.json"
-  options={{
-    nativeScrollbars: true,
-    theme: { colors: { primary: { main: '#dd5522' } } },
-  }}
-/>)
+  return (
+    <RedocStandalone
+      specUrl="http://rebilly.github.io/RebillyAPI/openapi.json"
+      options={{
+        nativeScrollbars: true,
+        theme: { colors: { primary: { main: '#dd5522' } } }
+      }}
+    />
+  );
 };
 
 export default App;
